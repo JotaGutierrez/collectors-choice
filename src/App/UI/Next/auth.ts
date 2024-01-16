3a1058f533f44837c634755862368dd69f62741e
Jota Gutierrez <jeliasgut@gmail.com>
2024-01-16 09:00:00 +0100

    Refactor auth config

--- src/App/UI/Next/auth.ts
+++ src/App/UI/Next/auth.ts
@@ -1,9 +1,13 @@
 import NextAuth from 'next-auth'
-import Google from 'next-auth/providers/google'
+import authConfig from './auth.config'
 
 export const {
   handlers: { GET, POST },
   auth
 } = NextAuth({
-  providers: [Google]
+  pages: {
+    signIn: '/login'
+  },
+  secret: process.env.NEXTAUTH_SECRET as string,
+  ...authConfig
 })
